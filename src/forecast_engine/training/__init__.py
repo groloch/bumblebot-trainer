f09028e110e0cb97e32ef10b52446472783a8162
groloch <bjuran2001@gmail.com>
2026-07-24 15:20:09 +0200

    added trainer factory

--- src/forecast_engine/training/__init__.py
+++ src/forecast_engine/training/__init__.py
@@ -1,1 +1,10 @@
 from .trainer import Trainer
+
+def build_trainer(config, config_path, type):
+    if type == 'pv':
+        from .trainers import PVTrainer
+        return PVTrainer(config, config_path)
+    elif type == 'ssl':
+        from .trainers import SSLTrainer
+        return SSLTrainer(config, config_path)
+    raise ValueError(f'Unknown trainer type: {type}')
