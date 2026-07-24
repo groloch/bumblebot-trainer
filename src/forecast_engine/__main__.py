20a4af60507d032443822b6c070c7341d371e687
groloch <bjuran2001@gmail.com>
2026-07-24 15:31:58 +0200

    updated config and config loading

--- src/forecast_engine/__main__.py
+++ src/forecast_engine/__main__.py
@@ -10,6 +10,14 @@ def main(config_path: str):
         # yaml doesnt load scientific notation as float
         config['training']['learning_rate'] = float(config['training']['learning_rate'])
 
+        match config['data']['encoding']:
+            case 'lc0':
+                config['model']['input_size'] = 112
+            case 'simplified':
+                config['model']['input_size'] = 18
+
+        config['model']['intermediate_size'] = config['model']['encoder']['intermediate_size']
+
     if config['type'] in ('pv', 'ssl'):
         from .training import build_trainer
         trainer = build_trainer(config, config_path, config['type'])
--- src/forecast_engine/config/data_config.py
+++ src/forecast_engine/config/data_config.py
@@ -3,12 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class DataConfig:
-    tablebase_path: str
-    fens_path: str
     datasets: list[str]
+    encoding: str
 
 
 @dataclass
 class SSLDataConfig:
     min_moves: int
     max_prediction_depth: int
+    encoding: str
