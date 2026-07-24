694b420ca9f9aaba604f6ff4596b30b3a482063c
groloch <bjuran2001@gmail.com>
2026-07-24 15:19:42 +0200

    application entrypoint

---
+++ src/forecast_engine/__main__.py
@@ -0,0 +1,26 @@
+import sys
+import yaml
+
+
+def main(config_path: str):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+
+    if 'training' in config:
+        # yaml doesnt load scientific notation as float
+        config['training']['learning_rate'] = float(config['training']['learning_rate'])
+
+    if config['type'] in ('pv', 'ssl'):
+        from .training import build_trainer
+        trainer = build_trainer(config, config_path, config['type'])
+
+        trainer.run()
+        trainer.wrapup()
+        
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python -m forecast_engine <config_path>")
+        sys.exit(1)
+    config_path = sys.argv[1]
+    main(config_path)
